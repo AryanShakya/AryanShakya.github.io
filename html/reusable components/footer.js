6f7bfa1e700a523687f6b89c6bffb5ef4b66a1a7
AryanShakya <105780539+AryanShakya@users.noreply.github.com>
2025-02-04 12:19:08 +0530

    Add files via upload

---
+++ html/reusable components/footer.js
@@ -0,0 +1,61 @@
+class Footer extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+      <style>
+      footer {
+        width:100%;
+        text-align: center;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+        overflow: hidden;
+        font-family: 'Raleway', sans-serif;
+        box-shadow: 0 1vw 2vw hsla(0, 0%, 0%, 0.9);
+        background-color: rgba(106, 106, 106, 0.3);
+        backdrop-filter: blur(14px);
+        color: #333;
+      }
+      footer a{
+        color: #333;
+      }
+
+      footer a:link {
+        color: #fff;
+        background-color: transparent;
+        text-decoration: underline;
+      }
+      
+      footer a:hover {
+        color: #fcf403;
+        background-color: transparent;
+        text-decoration: underline;
+      }
+      
+
+      img{
+        height:5vw;
+        width:5vw;
+      }
+
+      .footer{
+      }
+
+      </style>
+      <div class="cards">
+      ------------------The End------------------
+     </div>
+      <footer>
+      <div class="footer"> 
+      <b>Made with Love ;)</b><br>
+      <a href="https://github.com/AryanShakya/AryanShakya.github.io">source code</a><br>
+      </div>
+      </footer>
+
+      `;
+    }
+  }
+  customElements.define('footer-c', Footer);        
